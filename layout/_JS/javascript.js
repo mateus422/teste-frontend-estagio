737ced7bfe259931503df13978ad5b32d4bfd6be
Mateus4222 <mateus13.aa@hotmail.com>
2017-12-18 03:52:24 -0300

    V.0.5
    - JavaScript1.0 do Portfolio feito
    - Bugs corrigidos

--- layout/_JS/javascript.js
+++ layout/_JS/javascript.js
@@ -1,7 +1,85 @@
-
-jQuery(document).ready(function($) { 
-    $(".link").click(function(event){        
+/*Scroll do site*/
+jQuery(document).ready(function ($) {
+    $(".link").click(function (event) {
         event.preventDefault();
-        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 1000);
-   });
+        $('html,body').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 1000);
+    });
 });
+
+/*Movimentação do Portfolio*/
+jQuery(document).ready(
+
+    function ($) {
+        $(".buttonPort.all").click(
+            function () {
+                $(".figurePort.design").fadeIn();
+                $(".figurePort.identify").fadeIn();
+                $(".figurePort.photography").fadeIn();
+                $(".figurePort.ilustration").fadeIn();
+                $(".figurePort.central").fadeIn();
+            }
+        )
+    }
+);
+
+jQuery(document).ready(
+
+    function ($) {
+        $(".buttonPort.design").click(
+            function () {
+                $(".figurePort.design").fadeIn();
+                $(".figurePort.identify").fadeOut();
+                $(".figurePort.photography").fadeOut();
+                $(".figurePort.ilustration").fadeOut();
+                $(".figurePort.central").fadeOut();
+            }
+        )
+    }
+);
+
+jQuery(document).ready(
+
+    function ($) {
+        $(".buttonPort.identify").click(
+            function () {
+                $(".figurePort.design").fadeOut();
+                $(".figurePort.identify").fadeIn();
+                $(".figurePort.photography").fadeOut();
+                $(".figurePort.ilustration").fadeOut();
+                $(".figurePort.central").fadeOut();
+            }
+        )
+    }
+);
+
+jQuery(document).ready(
+
+    function ($) {
+        $(".buttonPort.photography").click(
+            function () {
+                $(".figurePort.design").fadeOut();
+                $(".figurePort.identify").fadeOut();
+                $(".figurePort.photography").fadeIn();
+                $(".figurePort.ilustration").fadeOut();
+                $(".figurePort.central").fadeOut();
+            }
+        )
+    }
+);
+
+jQuery(document).ready(
+
+    function ($) {
+        $(".buttonPort.ilustration").click(
+            function () {
+                $(".figurePort.design").fadeOut();
+                $(".figurePort.identify").fadeOut();
+                $(".figurePort.photography").fadeOut();
+                $(".figurePort.ilustration").fadeIn();
+                $(".figurePort.central").fadeOut();
+            }
+        )
+    }
+);
