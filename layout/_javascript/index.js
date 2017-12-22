34c7576b1815955d3ede93981d10473b5bdaa62f
Mateus4222 <mateus13.aa@hotmail.com>
2017-12-22 14:43:12 -0300

    V.0.7.3 (Versão final)
    -Últimos ajustes

--- layout/_javascript/index.js
+++ layout/_javascript/index.js
@@ -1,6 +1,9 @@
+ /*eslint-env jquery*/
+ /* eslint-disable eol-last */
+
 /*Scroll do site*/
 jQuery(document).ready(function ($) {
-    $(".link").click(function (event) {
+    $('.link').click(function (event) {
         event.preventDefault();
         $('html,body').animate({
             scrollTop: $(this.hash).offset().top
@@ -12,41 +15,41 @@ jQuery(document).ready(function ($) {
 /*Movimentação do Portfolio*/
 
 function showAll() {
-    $(".figurePort.design").fadeIn();
-    $(".figurePort.identify").fadeIn();
-    $(".figurePort.photography").fadeIn();
-    $(".figurePort.ilustration").fadeIn();
-    $(".figurePort.central").fadeIn();
+    $('.figurePort.design').fadeIn();
+    $('.figurePort.identify').fadeIn();
+    $('.figurePort.photography').fadeIn();
+    $('.figurePort.ilustration').fadeIn();
+    $('.figurePort.central').fadeIn();
 }
 
 function showDesign() {
-    $(".figurePort.design").fadeIn();
-    $(".figurePort.identify").fadeOut();
-    $(".figurePort.photography").fadeOut();
-    $(".figurePort.ilustration").fadeOut();
-    $(".figurePort.central").fadeOut();
+    $('.figurePort.design').fadeIn();
+    $('.figurePort.identify').fadeOut();
+    $('.figurePort.photography').fadeOut();
+    $('.figurePort.ilustration').fadeOut();
+    $('.figurePort.central').fadeOut();
 }
 
 function showIdentify() {
-    $(".figurePort.design").fadeOut();
-    $(".figurePort.identify").fadeIn();
-    $(".figurePort.photography").fadeOut();
-    $(".figurePort.ilustration").fadeOut();
-    $(".figurePort.central").fadeOut();
+    $('.figurePort.design').fadeOut();
+    $('.figurePort.identify').fadeIn();
+    $('.figurePort.photography').fadeOut();
+    $('.figurePort.ilustration').fadeOut();
+    $('.figurePort.central').fadeOut();
 }
 
 function showPhotography() {
-    $(".figurePort.design").fadeOut();
-    $(".figurePort.identify").fadeOut();
-    $(".figurePort.photography").fadeIn();
-    $(".figurePort.ilustration").fadeOut();
-    $(".figurePort.central").fadeOut();
+    $('.figurePort.design').fadeOut();
+    $('.figurePort.identify').fadeOut();
+    $('.figurePort.photography').fadeIn();
+    $('.figurePort.ilustration').fadeOut();
+    $('.figurePort.central').fadeOut();
 }
 
 function showIlustration() {
-    $(".figurePort.design").fadeOut();
-    $(".figurePort.identify").fadeOut();
-    $(".figurePort.photography").fadeOut();
-    $(".figurePort.ilustration").fadeIn();
-    $(".figurePort.central").fadeOut();
+    $('.figurePort.design').fadeOut();
+    $('.figurePort.identify').fadeOut();
+    $('.figurePort.photography').fadeOut();
+    $('.figurePort.ilustration').fadeIn();
+    $('.figurePort.central').fadeOut();
 }
